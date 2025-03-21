20953149cb0e66c946eaeacf4f2dd1e34ea2cba1
SashoFightera <alex0110.milanov@gmail.com>
2025-03-21 23:17:57 +0200

    SignUp update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,17 +1,9 @@
 import SignUpPage from "@/components/signup";
-import Component from "@/components/navigation_bar";
-import Footer from "@/components/footer";
-
-function Navigation() {
-  return <Component />;
-}
 
 function SignUp() {
   return (
     <>
-      <Navigation />
       <SignUpPage />
-      <Footer />
     </>
   );
 }
